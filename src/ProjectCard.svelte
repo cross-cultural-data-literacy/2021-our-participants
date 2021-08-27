aab2ab3108f54615a8d547d849af2a2ef0373490
Laurens <8708858+Razpudding@users.noreply.github.com>
2021-08-27 20:40:54 +0200

    failed attempt to style images in grid

--- src/ProjectCard.svelte
+++ src/ProjectCard.svelte
@@ -18,32 +18,53 @@
   }
 </script>
 
-<article on:click={handleClick}>
+<article class="cardContainer" on:click={handleClick}>
+  <div class="titleAndProject">
   <h1>Participant #{participant._id}</h1>
-  <a href={participant._project_link} target="blank">Link to final work</a>
-  {#if participantHas(participant, '_drawing_neighbourhood')}
-    <img src="{s.imageFolder + '/small/_drawing_neighbourhood' + participant._id + s.imageExtension}" alt="Drawing of the neighbourhood"/>
+  {#if participantHas(participant, '_project_link')}
+    <a href={participant._project_link} target="blank">A link to my project</a>
   {/if}
-  {#if participantHas(participant, '_drawing_room')}
-    <img src="{s.imageFolder + '/small/_drawing_room' + participant._id + s.imageExtension}" alt="Drawing of the participant's room"/>
-  {/if}
-  {#if participantHas(participant, '_photo_breakfast')}
-    <img src="{s.imageFolder + '/small/_photo_breakfast' + participant._id + s.imageExtension}" alt="Photo of the participant's breakfast"/>
-  {/if}
-  {#if participantHas(participant, '_photo_window')}
-    <img src="{s.imageFolder + '/small/_photo_window' + participant._id + s.imageExtension}" alt="Photo out of the participant's window"/>
-  {/if}
-  <p>I live in <span class=answer>{participant._city}</span></p>
-  <p>My main mode of transportation is <span class=answer>{participant._transportation}</span> {participant._transportation_emoji}</p>
-  <p>My favorite food is <span class=answer>{participant._favorite_food}</span></p>
-  {#if participant._housemates.toLowerCase().includes('alone')}
-    <p><span class=answer>I live by myself</span></p>
-  {:else}
-    <p>I live together with my <span class=answer>{participant._housemates.toLowerCase().replace('my', '')}</span> {participant._housemates_emoji}</p>
-  {/if}
-  {#if participantHas(participant, '_pet')}
-    <p>I have a <span class=answer>{participant._pet}!</span> {emojiGet(participant._pet)}</p>
-  {/if}
+  </div>
+
+    {#if participantHas(participant, '_drawing_neighbourhood')}
+      <figure>
+        <img src="{s.imageFolder + '/small/_drawing_neighbourhood' + participant._id + s.imageExtension}" alt="Drawing of the neighbourhood"/>
+        <figcaption>My neighbourhood.</figcaption>
+      </figure>  
+    {/if}
+    {#if participantHas(participant, '_drawing_room')}
+      <figure>
+        <img src="{s.imageFolder + '/small/_drawing_room' + participant._id + s.imageExtension}" alt="Drawing of the participant's room"/>
+        <figcaption>My room.</figcaption>
+      </figure> 
+    {/if}
+    {#if participantHas(participant, '_photo_breakfast')}
+      <figure>
+        <img src="{s.imageFolder + '/small/_photo_breakfast' + participant._id + s.imageExtension}" alt="Photo of the participant's breakfast"/>
+        <figcaption>My breakfast.</figcaption>
+      </figure>
+    {/if}
+    {#if participantHas(participant, '_photo_window')}
+      <figure>
+        <img src="{s.imageFolder + '/small/_photo_window' + participant._id + s.imageExtension}" alt="Photo out of the participant's window"/>
+        <figcaption>My view</figcaption>
+      </figure>
+    {/if}
+
+  
+  <div class="info">
+    <p>I live in <span class=answer>{participant._city}</span></p>
+    <p>My main mode of transportation is <span class=answer>{participant._transportation}</span> {participant._transportation_emoji}</p>
+    <p>My favorite food is <span class=answer>{participant._favorite_food}</span></p>
+    {#if participant._housemates.toLowerCase().includes('alone')}
+      <p><span class=answer>I live by myself</span></p>
+    {:else}
+      <p>I live together with my <span class=answer>{participant._housemates.toLowerCase().replace('my', '')}</span> {participant._housemates_emoji}</p>
+    {/if}
+    {#if participantHas(participant, '_pet')}
+      <p>I have a <span class=answer>{participant._pet}!</span> {emojiGet(participant._pet)}</p>
+    {/if}
+  </div>
 </article>
 
 <style>
@@ -52,12 +73,39 @@
     background-color: #f5efdf;
     width: 70%;
   }
-  img {
+  figure {
     max-width: 30%;
+  }
+  img {
+    /*max-width: 30%;*/
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    padding: 5px;
   }
   .answer {
     font-size: large;
     font-style: italic;
     text-decoration: rgb(113.36, 198.98, 107.02) underline;
   }
+
+  .cardContainer {
+    height: 100%;
+    margin: 0;
+    display: grid; 
+    grid-template-columns: 1fr 1fr 1fr 1fr; 
+    grid-template-rows: 1fr 1fr 1fr; 
+    gap: 0px 0px; 
+    grid-template-areas: 
+      "titleAndProject titleAndProject titleAndProject titleAndProject"
+      ". . . ."
+      "info info info info"; 
+  }
+  .titleAndProject { grid-area: titleAndProject; }
+  .info { grid-area: info; }
+
+
+/*  html, body , .container {
+    height: 100%;
+    margin: 0;
+  }*/
 </style> 
